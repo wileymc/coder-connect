1f1d85bcbb567c005edab03486c3e58ab636624c
Wiley Conte <wileymckayconte@gmail.com>
2022-12-22 02:15:36 -0700

    Add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:title" content="Coder Connect" />
+        <meta property="og:url" content="coderconnect.live" />
+        <meta
+          property="og:image"
+          content="https://coderconnect.live/preview.png"
+        />
+        <meta name="twitter:title" content="Coder Connect" />
+        <meta
+          name="twitter:description"
+          content="Gain valuable programming experience by contributing to open source projects"
+        />
+        <meta
+          name="twitter:image"
+          content="https://coderconnect.live/preview.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
